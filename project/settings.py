c22062e766a8618e9e1afc77e3d40c691bf39566
Muflone <webreg@muflone.com>
2023-03-08 00:21:45 +0100

    New application admin_settings

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_settings.apps.Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
